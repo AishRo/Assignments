5176d2014d92882bfd4dad9135c51b597a9b2166
Aishwarya Roy <roychoudhury.aishwarya@gmail.com>
2025-04-30 22:59:50 +0530

    Assignments

---
+++ src/assignment12_mathpow/MathPowerProblem.java
@@ -0,0 +1,18 @@
+package assignment12_mathpow;
+
+public class MathPowerProblem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 2.00000;
+		int n = -2;
+		
+		double z = Math.pow(x, n);
+		System.out.println(z);
+		
+		
+
+	}
+
+}
